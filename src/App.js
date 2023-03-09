e9ccd5120dcb59a71fad97f02e639f73e24bc218
이종호 <jongho03@naver.com>
2023-03-09 15:24:25 +0900

    2303_03_09_15

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import AppBar from "./components/Appbar/AppBar";
+import BoardBar from "./components/BoardBar/BoardBar";
+import BoardContent from "./components/BoardContent/BoardContent";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Helloasdasd</h1>
+    <div className="beautifuldnd-master">
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
       
     </div>
   );
